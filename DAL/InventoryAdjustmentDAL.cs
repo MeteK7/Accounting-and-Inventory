690bed65a0541c5dd40375b43f519d82b8b25878
mete <metekaba96@gmail.com>
2021-02-18 23:35:06 +0300

    Insert method has been composed in InventoryAdjustmentDAL.

--- DAL/InventoryAdjustmentDAL.cs
+++ DAL/InventoryAdjustmentDAL.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using KabaAccounting.CUL;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -53,6 +54,50 @@ namespace KabaAccounting.DAL
                     return dataTable;
                 }
             }
+        }
+        #endregion
+
+        #region INSERT METHOD
+        public bool Insert(InventoryAdjustmentCUL inventoryAdjustmentCUL)
+        {
+            bool isSuccess = false;
+
+            SqlConnection conn = new SqlConnection(connString);
+
+            try
+            {
+                string sqlQuery = "INSERT INTO tbl_pos (id, payment_type_id, customer_id, total_product_amount, cost_total, sub_total, vat, discount, grand_total, added_date, added_by) VALUES (@id, @payment_type_id, @customer_id, @total_product_amount, @cost_total, @sub_total, @vat, @discount, @grand_total, @added_date, @added_by)";
+
+                SqlCommand cmd = new SqlCommand(sqlQuery, conn);
+
+                cmd.Parameters.AddWithValue("@id", inventoryAdjustmentCUL.Id);//The column id in the database is not auto incremental. This is to prevent the number from increasing when the user deletes an existing invoice and creates a new invoice.
+                cmd.Parameters.AddWithValue("@total_product_amount", inventoryAdjustmentCUL.TotalProductAmount);
+                cmd.Parameters.AddWithValue("@grand_total", inventoryAdjustmentCUL.GrandTotal);
+                cmd.Parameters.AddWithValue("@added_date", inventoryAdjustmentCUL.AddedDate);
+                cmd.Parameters.AddWithValue("@added_by", inventoryAdjustmentCUL.AddedBy);
+
+                conn.Open();
+
+                int rows = cmd.ExecuteNonQuery();
+
+                if (rows > 0)
+                {
+                    isSuccess = true;
+                }
+                else
+                {
+                    isSuccess = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return isSuccess;
         }
         #endregion
     }
