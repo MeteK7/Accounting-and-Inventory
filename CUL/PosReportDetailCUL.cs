87ae794d220d8101d1b1780cf4f1d6a604d0a97a
mete <metekaba96@gmail.com>
2021-02-28 23:09:32 +0300

    PosReport page is under development.

---
+++ CUL/PosReportDetailCUL.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CUL
+{
+    public class PosReportDetailCUL
+    {
+        public int ReportId { get; set; }
+        public string ProductName { get; set; }
+        public int ProductAmountSold { get; set; }
+    }
+}
