1f5a4c116631be2750cbe60c3cc28d3b2dfaf275
Mete <metekaba96@gmail.com>
2021-03-19 19:30:30 +0300

    PointOfSaleBLL has been created.

---
+++ BLL/Interfaces/IPointOfSaleBLL.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Interfaces
+{
+    public interface IPointOfSaleBLL
+    {
+        public void LoadPastInvoice(int invoiceNo = 0, int invoiceArrow = -1);
+    }
+}
