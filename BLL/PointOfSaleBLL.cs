eb2e627badc4ed9ef64d25d9718df922aeb2b2a5
Mete <metekaba96@gmail.com>
2021-03-20 22:58:27 +0300

    RevertOldAmountInStock method is refactored.

--- BLL/PointOfSaleBLL.cs
+++ BLL/PointOfSaleBLL.cs
@@ -12,6 +12,9 @@ namespace BLL
     public class PointOfSaleBLL
     {
         PointOfSaleDAL pointOfSaleDAL = new PointOfSaleDAL();
+        ProductDAL productDAL = new ProductDAL();
+        ProductCUL productCUL = new ProductCUL();
+
 
         public bool InsertPOS(PointOfSaleCUL pointOfSaleCUL)
         {
@@ -22,5 +25,52 @@ namespace BLL
         {
             return pointOfSaleDAL.Update(pointOfSaleCUL);
         }
+
+        public void RevertOldAmountInStock(string[,] dgOldProductCells, int oldItemsRowCount)
+        {
+            int initialRowIndex = 0;
+            int colProductId = 0;
+            int colProductAmount = 5;
+            decimal productAmountFromDB;
+
+
+            DataTable dataTableProduct = new DataTable();
+
+            for (int rowNo = initialRowIndex; rowNo < oldItemsRowCount; rowNo++)
+            {
+                dataTableProduct = productDAL.SearchProductByIdBarcode(dgOldProductCells[rowNo, colProductId]);
+
+                productAmountFromDB = Convert.ToInt32(dataTableProduct.Rows[initialRowIndex]["amount_in_stock"]);
+
+                productCUL.AmountInStock = productAmountFromDB + Convert.ToDecimal(dgOldProductCells[rowNo, colProductAmount]);//Revert the amount in stock.
+
+                productCUL.Id = Convert.ToInt32(dgOldProductCells[rowNo, colProductId]);
+
+                productDAL.UpdateAmountInStock(productCUL);
+            }
+        }
+
+        //public string[,] GetDataGridContent(DataTable dgProducts)
+        //{
+        //    int rowLength = dgProducts.Rows.Count;
+        //    int colLength = 8;
+        //    string[,] dgProductCells = new string[rowLength, colLength];
+
+        //    for (int rowIndex = 0; rowIndex < rowLength; rowIndex++)
+        //    {
+        //        DataRow dgRow = dgProducts.Rows[rowIndex];
+
+        //        for (int colNo = 0; colNo < colLength; colNo++)
+        //        {
+        //            DataColumn contentCell = dgProducts.Columns[colNo];
+
+        //            dgProductCells[rowIndex, colNo] = contentCell.ToString();
+
+        //            //dgOldProductCells[rowNo, colNo] = cells[rowNo, colNo];//Assigning the old products' informations to the global array called "dgOldProductCells" so that we can access to the old products to revert the changes.
+        //        }
+        //    }
+
+        //    return dgProductCells;
+        //}
     }
 }
