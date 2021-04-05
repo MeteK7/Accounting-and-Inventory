ffd6d25e6f36e6d73be18f477bf14427f772aa58
Mete <metekaba96@gmail.com>
2021-04-05 13:58:44 +0300

    AccountDAL is created.

---
+++ DAL/AccountDAL.cs
@@ -0,0 +1,229 @@
+﻿using CUL;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class AccountDAL
+    {
+        static string connString = ConfigurationManager.ConnectionStrings["KabaAccountingConnString"].ConnectionString;
+
+
+        #region SELECT METHOD
+        public DataTable Select()
+        {
+            SqlConnection conn = new SqlConnection(connString);
+
+            DataTable dataTable = new DataTable();
+
+            try
+            {
+                string sqlQuery = "SELECT * FROM tbl_accounts";
+
+                SqlCommand cmd = new SqlCommand(sqlQuery, conn);
+                SqlDataAdapter dataAdapter = new SqlDataAdapter(cmd);
+                conn.Open();
+
+                dataAdapter.Fill(dataTable);
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return dataTable;
+        }
+        #endregion
+
+        #region INSERT METHOD
+        public bool Insert(AccountCUL accountCUL)
+        {
+            bool isSuccess = false;
+
+            SqlConnection conn = new SqlConnection(connString);
+
+            try
+            {
+                string sqlQuery = "INSERT INTO tbl_accounts (name, added_date, added_by) VALUES (@name, @added_date, @added_by)";
+
+                SqlCommand cmd = new SqlCommand(sqlQuery, conn);
+
+                cmd.Parameters.AddWithValue("name", accountCUL.Name);
+                cmd.Parameters.AddWithValue("added_date", accountCUL.AddedDate);
+                cmd.Parameters.AddWithValue("added_by", accountCUL.AddedBy);
+
+                conn.Open();
+
+                int rows = cmd.ExecuteNonQuery();
+
+                if (rows > 0)
+                {
+                    isSuccess = true;
+                }
+                else
+                {
+                    isSuccess = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return isSuccess;
+        }
+        #endregion
+
+        #region UPDATE METHOD
+        public bool Update(AccountCUL accountCUL)
+        {
+            bool isSuccess = false;
+
+            SqlConnection conn = new SqlConnection(connString);
+
+            try
+            {
+                string sqlQuery = "UPDATE tbl_accounts SET name=@name, added_date=@added_date, added_by=@added_by WHERE id=@id";
+
+                SqlCommand cmd = new SqlCommand(sqlQuery, conn);
+
+                cmd.Parameters.AddWithValue("name", accountCUL.Name);
+                cmd.Parameters.AddWithValue("added_date", accountCUL.AddedDate);
+                cmd.Parameters.AddWithValue("added_by", accountCUL.AddedBy);
+
+                conn.Open();
+
+                int rows = cmd.ExecuteNonQuery();
+
+                if (rows > 0)
+                {
+                    isSuccess = true;
+                }
+                else
+                {
+                    isSuccess = false;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return isSuccess;
+        }
+        #endregion
+
+        #region DELETE METHOD
+        public bool Delete(AccountCUL accountCUL)
+        {
+            //Create a Boolean variable and set its value to false.
+            bool isSuccess = false;
+
+            SqlConnection conn = new SqlConnection(connString);
+
+            try
+            {
+                //SQL Query to Delete from the Database
+                string sqlQuery = "DELETE FROM tbl_accounts WHERE id=@id";
+
+                SqlCommand cmd = new SqlCommand(sqlQuery, conn);
+
+                //Passing the value using cmd
+                cmd.Parameters.AddWithValue("@id", accountCUL.Id);
+
+                //Opening the SQL connection
+                conn.Open();
+
+                int rows = cmd.ExecuteNonQuery();
+
+                if (rows > 0)
+                {
+                    isSuccess = true;
+                }
+                else
+                {
+                    isSuccess = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return isSuccess;
+        }
+        #endregion
+
+        #region SEARCH METHOD
+        public DataTable Search(string keyword)
+        {
+            SqlConnection conn = new SqlConnection(connString);//Static method to connect database
+
+            DataTable dataTable = new DataTable();//To hold the data from database
+            try
+            {
+                String sql = "SELECT * FROM tbl_accounts WHERE id LIKE '%" + keyword + "%' OR name LIKE '%" + keyword + "%'";//SQL query to search data from database 
+                SqlCommand cmd = new SqlCommand(sql, conn);//For executing the command 
+                SqlDataAdapter dataAdapter = new SqlDataAdapter(cmd);//Getting data from database           
+                conn.Open();//Opening the database connection
+                dataAdapter.Fill(dataTable);//Passing values from adapter to Data Table
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return dataTable;
+        }
+        #endregion
+
+        #region SEARCH METHOD
+        public DataTable SearchById(int id)
+        {
+            SqlConnection conn = new SqlConnection(connString);//Static method to connect database
+
+            DataTable dataTable = new DataTable();//To hold the data from database
+            try
+            {
+                String sql = "SELECT * FROM tbl_accounts WHERE id=" + id + "";//SQL query to search data from database 
+                SqlCommand cmd = new SqlCommand(sql, conn);//For executing the command 
+                SqlDataAdapter dataAdapter = new SqlDataAdapter(cmd);//Getting data from database           
+                conn.Open();//Opening the database connection
+                dataAdapter.Fill(dataTable);//Passing values from adapter to Data Table
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return dataTable;
+        }
+        #endregion
+    }
+}
