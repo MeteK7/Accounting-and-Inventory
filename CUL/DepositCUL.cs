bbf2b6d5f6c0e929c8cfa8e155074a2b10638668
MeteK7 <metekaba96@gmail.com>
2021-04-13 22:21:30 +0300

    DepositCUL is developed.

---
+++ CUL/DepositCUL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CUL
+{
+    public class DepositCUL
+    {
+        public int Id { get; set; }
+        public int AccountId { get; set; }
+        public int BankId { get; set; }
+        public string Description { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime AddedDate { get; set; }
+        public int AddedBy { get; set; }
+    }
+}
--- DAL/DepositDAL.cs
+++ DAL/DepositDAL.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CUL;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
