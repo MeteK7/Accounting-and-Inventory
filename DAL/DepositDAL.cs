bf6214fb58fa36d0f4125fba26fee5e075504fff
MeteK7 <metekaba96@gmail.com>
2021-04-13 22:18:48 +0300

    DepositDAL is developed.

---
+++ DAL/DepositDAL.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class DepositDAL
+    {
+        static string connString = ConfigurationManager.ConnectionStrings["KabaAccountingConnString"].ConnectionString;
+
+
+        #region SELECT METHOD
+        public DataTable Select()
+        {
+            SqlConnection conn = new SqlConnection(connString);
+
+            DataTable dataTable = new DataTable();
+
+            try
+            {
+                string sqlQuery = "SELECT * FROM tbl_deposit";
+
+                SqlCommand cmd = new SqlCommand(sqlQuery, conn);
+                SqlDataAdapter dataAdapter = new SqlDataAdapter(cmd);
+                conn.Open();
+
+                dataAdapter.Fill(dataTable);
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return dataTable;
+        }
+        #endregion
+
+        #region INSERT METHOD
+        public bool Insert(DepositCUL depositCUL)
+        {
+            bool isSuccess = false;
+
+            SqlConnection conn = new SqlConnection(connString);
+
+            try
+            {
+                string sqlQuery = "INSERT INTO tbl_deposit (id, account_id, bank_id, description, amount, added_date, added_by) VALUES (@id, @account_id, @bank_id, @description, @amount, @added_date, @added_by)";
+
+                SqlCommand cmd = new SqlCommand(sqlQuery, conn);
+
+                cmd.Parameters.AddWithValue("@id", depositCUL.Id);//The column id in the database is not auto incremental. This is to prevent the number from increasing when the user deletes an existing invoice and creates a new invoice.
+                cmd.Parameters.AddWithValue("@account_id", depositCUL.AccountId);
+                cmd.Parameters.AddWithValue("@bank_id", depositCUL.BankId);
+                cmd.Parameters.AddWithValue("@description", depositCUL.Description);
+                cmd.Parameters.AddWithValue("@amount", depositCUL.Amount);
+                cmd.Parameters.AddWithValue("@added_date", depositCUL.AddedDate);
+                cmd.Parameters.AddWithValue("@added_by", depositCUL.AddedBy);
+
+                conn.Open();
+
+                int rows = cmd.ExecuteNonQuery();
+
+                if (rows > 0)
+                {
+                    isSuccess = true;
+                }
+                else
+                {
+                    isSuccess = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return isSuccess;
+        }
+        #endregion
+
+        #region UPDATE METHOD
+        public bool Update(DepositCUL depositCUL)
+        {
+            bool isSuccess = false;
+
+            SqlConnection conn = new SqlConnection(connString);
+
+            try
+            {
+                string sqlQuery = "UPDATE tbl_deposit SET id=@id, account_id=@account_id, bank_id=@bank_id, description=@decription, amount=@amount, added_date=@added_date, added_by=@added_by WHERE id=@id";
+
+                SqlCommand cmd = new SqlCommand(sqlQuery, conn);
+
+                cmd.Parameters.AddWithValue("id", depositCUL.Id);
+                cmd.Parameters.AddWithValue("account_id", depositCUL.AccountId);
+                cmd.Parameters.AddWithValue("bank_id", depositCUL.BankId);
+                cmd.Parameters.AddWithValue("description", depositCUL.Description);
+                cmd.Parameters.AddWithValue("amount", depositCUL.Amount);
+                cmd.Parameters.AddWithValue("added_date", depositCUL.AddedDate);
+                cmd.Parameters.AddWithValue("added_by", depositCUL.AddedBy);
+
+                conn.Open();
+
+                int rows = cmd.ExecuteNonQuery();
+
+                if (rows > 0)
+                {
+                    isSuccess = true;
+                }
+                else
+                {
+                    isSuccess = false;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return isSuccess;
+        }
+        #endregion
+
+        #region DELETE METHOD
+        public bool Delete(DepositCUL depositCUL)
+        {
+            //Create a Boolean variable and set its value to false.
+            bool isSuccess = false;
+
+            SqlConnection conn = new SqlConnection(connString);
+
+            try
+            {
+                //SQL Query to Delete from the Database
+                string sqlQuery = "DELETE FROM tbl_deposit WHERE id=@id";
+
+                SqlCommand cmd = new SqlCommand(sqlQuery, conn);
+
+                //Passing the value using cmd
+                cmd.Parameters.AddWithValue("@id", depositCUL.Id);
+
+                //Opening the SQL connection
+                conn.Open();
+
+                int rows = cmd.ExecuteNonQuery();
+
+                if (rows > 0)
+                {
+                    isSuccess = true;
+                }
+                else
+                {
+                    isSuccess = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return isSuccess;
+        }
+        #endregion
+    }
+}
