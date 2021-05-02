712712d4e047d85f4f1cbfb83f269277674206c2
Mete <metekaba96@gmail.com>
2021-05-02 23:12:52 +0300

    ExpenseBLL is created.

---
+++ BLL/ExpenseBLL.cs
@@ -0,0 +1,25 @@
+﻿using CUL;
+using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class ExpenseBLL
+    {
+        ExpenseDAL expenseDAL = new ExpenseDAL();
+
+        public bool Update(ExpenseCUL expenseCUL)
+        {
+            return expenseDAL.Insert(expenseCUL);
+        }
+
+        public bool Update(ExpenseCUL expenseCUL)
+        {
+            return expenseDAL.Update(expenseCUL);
+        }
+    }
+}
