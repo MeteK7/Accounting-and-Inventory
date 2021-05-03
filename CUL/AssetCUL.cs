b38df88331636fb21254fdb91049868d731a76d1
Mete <metekaba96@gmail.com>
2021-05-03 18:34:10 +0300

    cboAccount and cboBank are added.

---
+++ CUL/AssetCUL.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CUL
+{
+    public class AssetCUL
+    {
+        public int Id { get; set; }
+        public int AssetId { get; set; }
+        public string AssetType { get; set; }
+        public decimal Balance { get; set; }
+    }
+}
