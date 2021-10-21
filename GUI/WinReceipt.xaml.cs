0a3b4dffd667c4cd91d4724969035ee3bf61ad57
Mete <metekaba96@gmail.com>
2021-10-21 22:55:59 +0300

    WinReceipt has now objects.

--- GUI/WinReceipt.xaml.cs
+++ GUI/WinReceipt.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BLL;
+using DAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,6 +21,17 @@ namespace GUI
     /// </summary>
     public partial class WinReceipt : Window
     {
+        AccountDAL accountDAL = new AccountDAL();
+        SupplierDAL supplierDAL = new SupplierDAL();
+        UserBLL userBLL = new UserBLL();
+        ReceiptCUL receiptCUL = new ReceiptCUL();
+        ReceiptBLL receiptBLL = new ReceiptBLL();
+        ReceiptDAL receiptDAL = new ReceiptDAL();
+        AssetCUL assetCUL = new AssetCUL();
+        AssetDAL assetDAL = new AssetDAL();
+        BankDAL bankDAL = new BankDAL();
+        CommonBLL commonBLL = new CommonBLL();
+
         public WinReceipt()
         {
             InitializeComponent();
