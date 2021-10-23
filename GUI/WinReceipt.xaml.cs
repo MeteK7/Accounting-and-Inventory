bac7cba9603aba0b9925cd1f9f628af24f9de945
Mete <metekaba96@gmail.com>
2021-10-23 17:47:50 +0300

    LoadPastReceipt method is composed.

--- GUI/WinReceipt.xaml.cs
+++ GUI/WinReceipt.xaml.cs
@@ -3,6 +3,7 @@ using CUL;
 using DAL;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -33,7 +34,7 @@ namespace GUI
         BankDAL bankDAL = new BankDAL();
         CommonBLL commonBLL = new CommonBLL();
 
-        int initialIndex = 0, unitValue = 1;
+        const int initialIndex = 0, unitValue = 1;
         int clickedNewOrEdit, clickedNothing = -1, clickedNew = 0, clickedEdit = 1, clickedNull = 2;//0 stands for user clicked the button New, and 1 stands for user clicked the button Edit.
         int account = 1, bank = 2, supplier = 3;
         int clickedArrow, clickedPrev = 0, clickedNext = 1;
@@ -47,6 +48,13 @@ namespace GUI
         public WinReceipt()
         {
             InitializeComponent();
+            DisableTools();
+            LoadPastReceipt();
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
 
         private void FirstTimeRun()
@@ -94,18 +102,80 @@ namespace GUI
             rbBank.IsEnabled = true;
         }
 
-        private void btnClose_Click(object sender, RoutedEventArgs e)
+        //-1 means user did not clicked either previous or next button which means user just clicked the point of purchase button to open it.
+        private void LoadPastReceipt(int receiptId = initialIndex, int receiptArrow = -unitValue)//Optional parameter
         {
-            this.Close();
+            int idAssetFrom;
+
+            if (receiptId == initialIndex)//If the ID is 0 came from the optional parameter, that means user just clicked the WinPOP button to open it.
+            {
+                receiptId = commonBLL.GetLastRecordById(calledBy);//Getting the last invoice id and assign it to the variable called invoiceId.
+            }
+
+            /*WE CANNOT USE ELSE IF FOR THE CODE BELOW! BOTH IF STATEMENTS ABOVE AND BELOVE MUST WORK.*/
+            if (receiptId != initialIndex)// If the invoice number is still 0 even when we get the last invoice number by using code above, that means this is the first sale and do not run this code block.
+            {
+                DataTable dtReceipt = receiptDAL.GetByReceiptId(receiptId);
+
+                if (dtReceipt.Rows.Count != initialIndex)
+                {
+                    receiptId = Convert.ToInt32(dtReceipt.Rows[initialIndex][colTxtId].ToString());//Getting the id of account.
+                    lblReceiptId.Content = receiptId;
+
+                    #region ASSET INFORMATION FILLING REGION
+                    idAssetFrom = Convert.ToInt32(dtReceipt.Rows[initialIndex]["id_asset_from"].ToString());
+                    //lblAssetIdFrom.Content = idAssetFrom;
+
+                    DataTable dtAsset = assetDAL.SearchById(idAssetFrom);
+                    int sourceType = Convert.ToInt32(dtAsset.Rows[initialIndex]["id_source_type"]);
+
+                    if (sourceType == account)
+                        rbAccount.IsChecked = true;
+                    else
+                        rbBank.IsChecked = true;
+
+                    LoadCboFrom(sourceType);//This function works twice when you open the WinReceipt because the rb selection is being changed. But if the previous selection is same, rbBank_Checked does not work so the method LoadCboFrom called by rbBank_Checked does not work as well.
+                    cboFrom.SelectedValue = dtAsset.Rows[initialIndex]["id_source"].ToString();
+                    #endregion
+
+                    LoadCboTo();
+                    cboTo.SelectedValue = Convert.ToInt32(dtReceipt.Rows[initialIndex][colTxtIdTo].ToString());//Getting the id of supplier.
+
+                    txtAmount.Text = dtReceipt.Rows[initialIndex][colTxtAmount].ToString();
+                    txtDetails.Text = dtReceipt.Rows[initialIndex][colTxtDetails].ToString();
+                    lblDateAdded.Content = Convert.ToDateTime(dtReceipt.Rows[initialIndex][colTxtAddedDate]).ToString("f");
+                }
+                else if (dtReceipt.Rows.Count == initialIndex)//If the pop detail row quantity is 0, that means there is no such row so decrease or increase the invoice number according to user preference.
+                {
+                    if (receiptArrow == initialIndex)//If the invoice arrow is 0, that means user clicked the previous button.
+                    {
+                        receiptId = receiptId - unitValue;
+                    }
+                    else
+                    {
+                        receiptId = receiptId + unitValue;
+                    }
+
+                    if (receiptArrow != -unitValue)//If the user has not clicked either previous or next button, then the invoiceArrow will be -1 and no need for recursion.
+                    {
+                        LoadPastReceipt(receiptId, receiptArrow);//Call the method again to get the new past invoice.
+                    }
+
+                }
+            }
+            else
+            {
+                FirstTimeRun();//This method is called when it is the first time of using this program.
+            }
         }
 
         private void btnMenuNew_Click(object sender, RoutedEventArgs e)
         {
-            int lastExpenseId = commonBLL.GetLastRecordById(calledBy), currentInvoiceId;
+            int lastReceiptId = commonBLL.GetLastRecordById(calledBy), currentInvoiceId;
 
             currentInvoiceId = Convert.ToInt32(lblReceiptId.Content);
 
-            if (currentInvoiceId != lastExpenseId)
+            if (currentInvoiceId != lastReceiptId)
             {
                 int nextInvoice = currentInvoiceId + unitValue;
 
