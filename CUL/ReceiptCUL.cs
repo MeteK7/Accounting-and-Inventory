e3a5b18d31e689adc65efbb6c668c81c855e92df
Mete <metekaba96@gmail.com>
2021-10-24 21:18:16 +0300

    ReceiptCUL is developed.

--- CUL/ReceiptCUL.cs
+++ CUL/ReceiptCUL.cs
@@ -8,5 +8,15 @@ namespace CUL
 {
     public class ReceiptCUL
     {
+        public int Id { get; set; }
+        public int IdFrom { get; set; }
+        public int IdTo { get; set; }
+        public int IdAssetFrom { get; set; }
+        public int IdAssetTo { get; set; }
+        public int AssetId { get; set; }
+        public decimal Amount { get; set; }
+        public string Details { get; set; }
+        public int AddedBy { get; set; }
+        public DateTime AddedDate { get; set; }
     }
 }
