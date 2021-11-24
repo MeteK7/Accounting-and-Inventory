1d88f4530be351ab4aa1841362267c94d7924c73
Mete <metekaba96@gmail.com>
2021-11-24 20:29:47 +0300

    PopReportDetailCUL class is developed.

---
+++ CUL/PopReportDetailCUL.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CUL
+{
+    public class PopReportDetailCUL
+    {
+        public int InvoiceId { get; set; }
+        public int PurchaseDateId { get; set; }
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }//Filled by ProductDAL.
+        public decimal ProductQuantityPurchased { get; set; }
+        public string ProductTotalCostPrice { get; set; }
+        public string UserFullName { get; set; }
+        public decimal UserSaleAmount { get; set; }
+    }
+}
