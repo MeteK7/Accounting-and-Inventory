87fd017054efc644fb79bebe502bf418971743da
Mete <metekaba96@gmail.com>
2021-11-25 21:55:16 +0300

    Insert method is modified.

--- DAL/PointOfSaleDetailDAL.cs
+++ DAL/PointOfSaleDetailDAL.cs
@@ -54,7 +54,7 @@ namespace DAL
 
             try
             {
-                String sqlQuery = "INSERT INTO tbl_pos_detailed (id, product_id, product_unit_id, added_by, rate, quantity, product_cost_price, product_sale_price) VALUES (@id, @product_id, @product_unit_id, @added_by, @rate, @quantity, @product_cost_price, @product_sale_price)";
+                String sqlQuery = "INSERT INTO tbl_pos_detailed (id, product_id, product_unit_id, added_by, rate, quantity, product_sale_price, product_discount, product_vat) VALUES (@id, @product_id, @product_unit_id, @added_by, @rate, @quantity, @product_cost_price, @product_sale_price, @product_discount, @product_vat)";
 
                 SqlCommand cmd = new SqlCommand(sqlQuery, conn);
 
@@ -66,6 +66,8 @@ namespace DAL
                 cmd.Parameters.AddWithValue("@quantity", pointOfSaleDetailCUL.ProductQuantity);
                 cmd.Parameters.AddWithValue("@product_cost_price", pointOfSaleDetailCUL.ProductCostPrice);
                 cmd.Parameters.AddWithValue("@product_sale_price", pointOfSaleDetailCUL.ProductSalePrice);
+                cmd.Parameters.AddWithValue("@product_discount", pointOfSaleDetailCUL.ProductDiscount);
+                cmd.Parameters.AddWithValue("@product_vat", pointOfSaleDetailCUL.ProductVAT);
 
                 conn.Open();
 
