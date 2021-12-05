1144e044dd29d36d13a912b74069f4ac936f304c
Mete <metekaba96@gmail.com>
2021-12-05 10:35:11 +0300

    Enums for Inventory Adjustment page are created.

---
+++ CUL/Enums/InvAdjColumns.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CUL.Enums
+{
+    public enum InvAdjColumns
+    {
+        ColProductId,
+        ColProductName,
+        ColProductUnit,
+        ColProductCostPrice,
+        ColProductSalePrice,
+        ColProductQuantityInReal,
+        ColProductQuantityInStock,
+        ColProductQuantityDifference,
+        ColProductTotalCostPrice,
+        ColProductTotalSalePrice
+    }
+}
