8b27796f2d9f2d80a2c1b79905c9d8a20e9dc6c1
Mete <metekaba96@gmail.com>
2021-12-29 22:50:23 +0300

    FileExtensions enum is created.

---
+++ CUL/Enums/FileExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CUL.Enums
+{
+    public enum FileExtensions
+    {
+        [Description(".bak")]
+        bak,
+        [Description(".txt")]
+        txt
+    }
+}
--- GUI/WinBackup.xaml.cs
+++ GUI/WinBackup.xaml.cs
@@ -40,7 +40,7 @@ namespace GUI
         private void btnBackup_Click(object sender, RoutedEventArgs e)
         {
             backupCUL.DatabasePath = lblPath.Content.ToString();
-            backupCUL.DatabaseName = "AccountingAndInventory" + DateTime.Now.ToString("M-dd-yyyy-HH-mm-ss");
+            backupCUL.DatabaseName = "AccountingAndInventory" + DateTime.Now.ToString("M-dd-yyyy-HH-mm-ss")+".bak";
             string isSuccess=backupDAL.BackupDatabase(backupCUL);
 
             MessageBox.Show(isSuccess);
