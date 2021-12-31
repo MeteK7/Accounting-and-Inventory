24ccd521d48e5c4a667ad437957b86d91bebc341
Mete <metekaba96@gmail.com>
2021-12-31 22:32:18 +0300

    DataGridProduct is created.

---
+++ CUL/Constants/DataGrid/DataGridProduct.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CUL.Constants.DataGrid
+{
+    public static class DataGridProduct
+    {
+        public const string Id = "dgTxtProductId";
+        public const string Name = "dgTxtProductName";
+        public const string Unit = "dgTxtProductUnit";
+    }
+}
